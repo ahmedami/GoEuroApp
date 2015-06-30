95e1624669f67e6c6e38f65311e035f6f3acd8a7
ahmedami <eng.ahmedami@gmail.com>
2015-06-30 10:15:50 +0200

    commit documentation.

--- GoEuroApp/src/main/java/com/goeuro/Test.java
+++ GoEuroApp/src/main/java/com/goeuro/Test.java
@@ -9,7 +9,9 @@ package com.goeuro;
  *
  * @author ahmed
  */
-
+import com.goeuro.customexception.CustomException;
+import com.goeuro.dao.Imp.CheckExistanceImp;
+import com.goeuro.dto.PlaceDto;
 import com.goeuro.facade.Imp.ControllerImp;
 
 
@@ -18,7 +20,7 @@ public class Test {
     public static void main(String[] args) {
         String placeName=" ";
         if(args.length!=0)
-         ll   placeName=args[0];
+            placeName=args[0];
         ControllerImp controller = new ControllerImp();
         controller.run(placeName);
     }
